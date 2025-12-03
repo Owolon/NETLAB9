d0a5c9a59b487d6673f924d1b29e0bb7bdc687a1
Owolon <keepoutmind@gmail.com>
2025-12-04 02:57:07 +0300

    Add files via upload

---
+++ NET_LAB9W/NET_LAB9W/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace NET_LAB9W
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            panel3 = new Panel();
+            button6 = new Button();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            panel4 = new Panel();
+            textBox5 = new TextBox();
+            label4 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MediumAquamarine;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(13, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Создать файл";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(4, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Количество чисел:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(4, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Имя файла:";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(13, 9);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(330, 40);
+            panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(150, 10);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(169, 25);
+            textBox1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(12, 61);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(330, 40);
+            panel2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(102, 10);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(218, 25);
+            textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightBlue;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(348, 66);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 30);
+            button2.TabIndex = 5;
+            button2.Text = "Обновить";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button6);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(label3);
+            panel3.Location = new Point(13, 148);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(464, 72);
+            panel3.TabIndex = 4;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LightBlue;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button6.Location = new Point(335, 31);
+            button6.Name = "button6";
+            button6.Size = new Size(121, 30);
+            button6.TabIndex = 9;
+            button6.Text = "Вставить";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.HighlightText;
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(4, 32);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(326, 29);
+            textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(4, 8);
+            label3.Name = "label3";
+            label3.Size = new Size(221, 21);
+            label3.TabIndex = 1;
+            label3.Text = "Введите последовательность:";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.Azure;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox4.Location = new Point(13, 318);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.ScrollBars = ScrollBars.Both;
+            textBox4.Size = new Size(449, 169);
+            textBox4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightBlue;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button3.Location = new Point(12, 226);
+            button3.Name = "button3";
+            button3.Size = new Size(193, 35);
+            button3.TabIndex = 6;
+            button3.Text = "Показать содержимое";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(textBox5);
+            panel4.Controls.Add(label4);
+            panel4.Location = new Point(12, 267);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(457, 40);
+            panel4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox5.Location = new Point(57, 10);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(396, 25);
+            textBox5.TabIndex = 3;
+            textBox5.Text = "Файл ещё не создан";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(4, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 21);
+            label4.TabIndex = 2;
+            label4.Text = "Путь:";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.LightBlue;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button4.Location = new Point(13, 504);
+            button4.Name = "button4";
+            button4.Size = new Size(142, 30);
+            button4.TabIndex = 7;
+            button4.Text = "Экспорт файла";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.LightBlue;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button5.Location = new Point(161, 504);
+            button5.Name = "button5";
+            button5.Size = new Size(142, 30);
+            button5.TabIndex = 8;
+            button5.Text = "Импорт файла";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.LightBlue;
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button7.Location = new Point(220, 107);
+            button7.Name = "button7";
+            button7.Size = new Size(249, 35);
+            button7.TabIndex = 9;
+            button7.Text = "Вывести информацио о файле";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.PaleVioletRed;
+            button8.FlatStyle = FlatStyle.Popup;
+            button8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button8.Location = new Point(370, 504);
+            button8.Name = "button8";
+            button8.Size = new Size(81, 30);
+            button8.TabIndex = 10;
+            button8.Text = "Сброс";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(481, 556);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(panel4);
+            Controls.Add(button3);
+            Controls.Add(textBox4);
+            Controls.Add(panel3);
+            Controls.Add(button2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Лабоработрная работа 9. Файлы";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private TextBox textBox1;
+        private Panel panel2;
+        private TextBox textBox2;
+        private Button button2;
+        private Panel panel3;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Button button3;
+        private Panel panel4;
+        private TextBox textBox5;
+        private Label label4;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+    }
+}
---
+++ NET_LAB9W/NET_LAB9W/Form1.cs
@@ -0,0 +1,345 @@
+﻿namespace NET_LAB9W
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("Нет активного файла для экспорта!");
+                    return;
+                }
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Бинарные файлы (*.bin)|*.bin";
+                sfd.FileName = Path.GetFileName(filePath);
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    File.Copy(filePath, sfd.FileName, true);
+                    MessageBox.Show("Файл успешно экспортирован!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка экспорта: " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Бинарные файлы (*.bin)|*.bin|Все файлы (*.*)|*.*";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string projectDir = GetProjectDirectory();
+                    string fileName = Path.GetFileName(ofd.FileName);
+                    string newPath = Path.Combine(projectDir, fileName);
+
+                    if (File.Exists(newPath))
+                        File.Delete(newPath);
+
+                    using (FileStream source = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (FileStream dest = new FileStream(newPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        source.CopyTo(dest);
+                    }
+
+                    filePath = newPath;
+                    textBox2.Text = Path.GetFileNameWithoutExtension(fileName);
+                    textBox5.Text = "Файл: " + filePath;
+
+                    MessageBox.Show("Файл успешно импортирован и установлен как активный!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка импорта: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("Файл не найден!");
+                    return;
+                }
+
+                textBox4.Clear();
+                List<double> numbers = new List<double>();
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    while (reader.BaseStream.Position < reader.BaseStream.Length)
+                    {
+                        numbers.Add(reader.ReadDouble());
+                    }
+                }
+
+                for (int i = 0; i < numbers.Count; i++)
+                    textBox4.AppendText($"{i}: {numbers[i]}\r\n");
+
+                MessageBox.Show("Чтение завершено.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка чтения: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!int.TryParse(textBox1.Text, out int count) || count <= 0)
+                {
+                    MessageBox.Show("Введите корректное количество чисел.");
+                    return;
+                }
+
+                string fileName = textBox2.Text.Trim();
+                if (string.IsNullOrWhiteSpace(fileName))
+                {
+                    MessageBox.Show("Введите имя файла!");
+                    return;
+                }
+
+                fileName += ".bin";
+
+                string projectDir = GetProjectDirectory();
+                filePath = Path.Combine(projectDir, fileName);
+
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                using (BinaryWriter writer = new BinaryWriter(fs))
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        double value = rnd.NextDouble() * rnd.Next(-5000, 5000) * Math.Sin(rnd.NextDouble());
+                        value = Math.Round(value, 2);
+                        writer.Write(value);
+                    }
+                }
+
+                textBox5.Text = "Файл: " + filePath;
+                MessageBox.Show("Файл успешно создан!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+
+        }
+        private string GetProjectDirectory()
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo di = new DirectoryInfo(dir);
+
+            while (di != null && di.GetFiles("*.csproj").Length == 0)
+                di = di.Parent;
+
+            return di?.FullName ?? AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("Сначала создайте файл!");
+                    return;
+                }
+
+                if (!double.TryParse(textBox3.Text, out double number))
+                {
+                    MessageBox.Show("Введите корректное число.");
+                    return;
+                }
+
+                List<double> numbers = new List<double>();
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    while (reader.BaseStream.Position < reader.BaseStream.Length)
+                        numbers.Add(reader.ReadDouble());
+                }
+
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+                int pos = rnd.Next(0, numbers.Count);
+
+                numbers.Insert(pos, number);
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                using (BinaryWriter writer = new BinaryWriter(fs))
+                {
+                    foreach (double n in numbers)
+                        writer.Write(n);
+                }
+
+                MessageBox.Show($"Число {number} вставлено в позицию {pos}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string inputName = textBox2.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(inputName))
+                {
+                    MessageBox.Show("Введите новое имя файла!");
+                    return;
+                }
+
+                if (!inputName.EndsWith(".bin", StringComparison.OrdinalIgnoreCase))
+                    inputName += ".bin";
+
+                string projectDir = GetProjectDirectory();
+                string oldFullPath = filePath;
+                string newFullPath = Path.Combine(projectDir, inputName);
+
+                if (string.Equals(Path.GetFileName(oldFullPath), inputName, StringComparison.OrdinalIgnoreCase))
+                {
+                    MessageBox.Show($"Имя файла уже установлено: {inputName}\nПуть: {oldFullPath}");
+                    return;
+                }
+
+                if (File.Exists(oldFullPath))
+                {
+                    DialogResult renameResult = MessageBox.Show(
+                        $"Файл с текущим именем найден:\n{oldFullPath}\n\nПереименовать текущий файл в новое имя?",
+                        "Подтверждение",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+
+                    if (renameResult == DialogResult.Yes)
+                    {
+                        if (File.Exists(newFullPath))
+                        {
+                            DialogResult overwriteResult = MessageBox.Show(
+                                $"Файл с именем {inputName} уже существует. Перезаписать?",
+                                "Подтверждение",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Warning);
+
+                            if (overwriteResult == DialogResult.Yes)
+                                File.Delete(newFullPath);
+                            else
+                                return;
+                        }
+
+                        File.Move(oldFullPath, newFullPath);
+                        filePath = newFullPath;
+                        MessageBox.Show($"Файл переименован.\nНовое имя: {inputName}\nПуть: {filePath}");
+                    }
+                    else
+                    {
+                        string oldName = Path.GetFileName(filePath);
+                        filePath = newFullPath;
+                        textBox5.Text = "Файл: " + filePath;
+                        MessageBox.Show($"Имя файла изменено (без переименования старого файла):\n{oldName} → {inputName}");
+                    }
+                }
+                else
+                {
+                    filePath = newFullPath;
+                    textBox5.Text = "Файл: " + filePath;
+                    MessageBox.Show($"Имя файла установлено: {inputName}\nПуть: {filePath}");
+                }
+
+                textBox2.Text = Path.GetFileNameWithoutExtension(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка изменения имени файла: " + ex.Message);
+            }
+        }
+
+        private void ShowFileInfo()
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("Файл не найден!");
+                    return;
+                }
+
+                FileInfo info = new FileInfo(filePath);
+
+                string fileDetails =
+                    $"Имя: {info.Name}\r\n" +
+                    $"Путь: {info.FullName}\r\n" +
+                    $"Размер: {info.Length} байт\r\n" +
+                    $"Создан: {info.CreationTime}\r\n" +
+                    $"Последний доступ: {info.LastAccessTime}\r\n" +
+                    $"Последняя запись: {info.LastWriteTime}\r\n";
+
+                MessageBox.Show(fileDetails, "Информация о файле");
+                string infoPath = Path.Combine(GetProjectDirectory(), "file_info.txt");
+
+                File.WriteAllText(infoPath, fileDetails);
+
+                MessageBox.Show($"Информация о файле сохранена в:\n{infoPath}", "Сохранено");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка получения данных: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ShowFileInfo();
+        }
+
+        private void ResetAll()
+        {
+            try
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+
+                filePath = "";
+
+                MessageBox.Show("Все поля и активный файл сброшены.", "Сброс выполнен", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при сбросе: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ResetAll();
+        }
+    }
+}
